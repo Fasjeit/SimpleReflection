a65c79436e008cd0288cf8785bc8c01c34bc6cd0
Stanislav Silin <silin.stanislav@outlook.com>
2019-05-08 22:18:10 +0300

    Added stub extention

---
+++ SimpleReflection/SimpleReflection/StubExtention.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static class StubExtention
+{
+    public static Dictionary<string, Type> GetBakedType<TValue>(this TValue value)
+    {
+        return new Dictionary<string, Type>();
+    }
+}
